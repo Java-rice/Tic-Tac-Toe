b28ebafdb25d5a042218af563ea8c65c993e6e95
Java-rice <johnmarkpperoche@iskolarngbayan.pup.edu.ph>
2023-09-03 13:14:52 +0800

    pages

---
+++ src/main.py
@@ -0,0 +1,43 @@
+import tkinter as tk 
+import customtkinter
+from PIL import Image, ImageTk
+from views import about, home, welcome
+
+
+class mainpage(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+
+        #clip the main window in the center
+        width = 600
+        height = 700
+        s_width = self.winfo_screenwidth()
+        s_height = self.winfo_screenheight()
+        x = (s_width/2) - (width/2)
+        y = (s_height/2) - (height/2)
+        
+        #main-window
+        self.wm_title("Tic Tac Toe")
+        self.geometry("%dx%d+%d+%d" % (width, height, x, y))
+        self.resizable(0,0)
+        
+        #main-container        
+        container =  customtkinter.CTkFrame(self)
+        container.place(relx=.5, rely=.5, anchor="center")
+        
+        #frames
+        self.frames = {}
+        for F in (welcome.startpage, about.aboutpage, home.homepage):
+            frame = F(container,self)
+            self.frames[F] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
+        self.show_frame(welcome.startpage)
+    
+    #raise the current frame to the top
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()     
+        
+if __name__ == "__main__":
+    App = mainpage()
+    App.mainloop()
---
+++ src/views/welcome.py
@@ -0,0 +1,7 @@
+
+import tkinter as tk
+
+class startpage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        pass
